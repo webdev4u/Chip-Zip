eed8080fc2baa078b156471f899b52ff2e8fb902
Webdev4u <webdev4you@gmail.com>
2012-10-10 18:43:20 +0400

    Removed Top from pages menu

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Page.create(link: 'top', title: 'Меню', parent_id: nil)
